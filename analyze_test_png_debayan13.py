cec9ea057dad75b5ff905a29eafefc95ea6f17d3
Debayan Banerjee <debayanin@gmail.com>
2018-12-28 12:28:20 +0100

    Resize prediction instead of gt and calculate separate IoUs

--- analyze_test_png_debayan13.py
+++ analyze_test_png_debayan13.py
@@ -6,36 +6,41 @@ import torch
 import math
 from PIL import Image
 
-parser = argparse.ArgumentParser(description="Discovering Class Specific Pixels (DCSP) Network.")
-parser.add_argument('--source', type=str, default='out_la_crf_aff')
+cocolabels = ['bg','person','bicycle','car','motorcycle','airplane','bus','train','truck','boat','traffic light','fire hydrant','stop sign','parking meter','bench','bird','cat','dog','horse','sheep','cow','elephant','bear','zebra','giraffe','backpack','umbrella','handbag','tie','suitcase','frisbee','skis','snowboard','sports ball','kite','baseball bat','baseball glove','skateboard','surfboard','tennis racket','bottle','wine glass','cup','fork','knife','spoon','bowl','banana','apple','sandwich','orange','broccoli','carrot','hot dog','pizza','donut','cake','chair','couch','potted plant','bed','dining table','toilet','tv','laptop','mouse','remote','keyboard','cell phone','microwave','oven','toaster','sink','refrigerator','book','clock','vase','scissors','teddy bear','hair drier','toothbrush']
+
+parser = argparse.ArgumentParser(description="DVSE Engilberge IoU evaluation by johann/debayan")
+parser.add_argument('--thresh', type=float, default=0.5)
 args = parser.parse_args()
 
 img_gt_name_list = open('smallval.txt').read().splitlines()
 img_name_list = [img[:-4] for img in img_gt_name_list]
 
-totalintersection = 0
-totalunion = 0
-catg_seen = set()
+totalintersection = np.zeros((81))
+totalunion = np.zeros((81))
 for line_counter, img_name in enumerate(img_name_list):
-    gt1 = misc.imread('./data/ClassSegmentation/val2014/'+img_name+'.png')
-    gt = misc.imresize(gt1,(13,13))
-    for val in np.unique(gt):
-        if val not in gt1:
-            gt[gt==val] = 0
-    pred = np.load('./numpyserialised13/'+img_name+'.npz')['arr_0']
+    gt = misc.imread('./data/ClassSegmentation/val2014/'+img_name+'.png')
+    pred = np.load('./numpyserialised13/'+img_name+'.npz')['arr_0'] #This is 81 channels thick, each channel 13x13. channel 0 = 0, other channels have values from -inf to +inf, each channel belonging to a coco class 
     for i in range(13):
         for j in range(13):
             torchx = torch.from_numpy(pred[:,i,j])
-            m = torch.nn.Softmax()
+            m = torch.nn.Sigmoid()
             sigmoidtorchx = m(torchx)
             pred[:,i,j] = sigmoidtorchx.cpu().numpy()
-    pred[0] = 0.25
-    pred = np.argmax(pred,0)
+    pred[0] = args.thresh
+    pred1c = np.argmax(pred,0) #pred is 81 channels, pred1c is 1 channel
+    pred1cresized = np.resize(pred1c,gt.shape)
     for i in range(81):
-        intersection = np.sum(np.logical_and(np.equal(gt, i), np.equal(pred, i)))
-        union = np.sum(np.logical_or(np.equal(gt, i), np.equal(pred, i)))
-        totalintersection += intersection
-        totalunion += union
+        totalintersection[i] += np.sum(np.logical_and(np.equal(gt, i), np.equal(pred1cresized, i)))
+        totalunion[i] += np.sum(np.logical_or(np.equal(gt, i), np.equal(pred1cresized, i)))
     if line_counter%100 == 99:
-        print("%d images, IoU: %f"%(line_counter,(totalintersection/float(totalunion))))
-print("%d images, IoU: %f"%(line_counter,(totalintersection/float(totalunion))))
+        iou = np.sum(np.divide(totalintersection, totalunion+0.000001))/81.0
+        for l in range(81):
+            print("%s = %f"%(cocolabels[l], totalintersection[l]/float(totalunion[l])),end=' ')
+        print("%d images, IoU: %f"%(line_counter, iou))
+        print("%d classes still not seen"%(81-np.count_nonzero(totalintersection)))
+
+iou = np.sum(np.divide(totalintersection, totalunion+0.000001))/81.0
+for l in range(81):
+    print("%s = %f"%(cocolabels[l], totalintersection[l]/float(totalunion[l])),end=' ')
+print("%d images, IoU: %f"%(line_counter, iou))
+print("%d classes still not seen"%(81-np.count_nonzero(totalintersection)))
